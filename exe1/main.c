50c6bad4712417b392e378706564805eb051e14d
Giulia <giuliar1@al.insper.edu.br>
2025-02-20 02:15:51 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
+volatile int event_flag = 0;  
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
-    printf("fall \n");
-  } else if (events == 0x8) { // rise edge
-    printf("rise \n");
+  if (events == 0x4) { // borda de descida (fall edge)
+    event_flag = 1;
+  } else if (events == 0x8) { // borda de subida (rise edge)
+    event_flag = 2;
   }
 }
 
@@ -23,5 +24,14 @@ int main() {
       BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
   while (true) {
+    if (event_flag != 0) {
+      if (event_flag == 1) {
+        printf("fall\n");
+      } else if (event_flag == 2) {
+        printf("rise\n");
+      }
+      event_flag = 0;  // reseta a flag após tratar o evento
+    }
   }
+  return 0;
 }
