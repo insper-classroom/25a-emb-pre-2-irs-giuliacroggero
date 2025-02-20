1d4e5df8af9b9312678eab17317cae589a0c93ec
Giulia <giuliar1@al.insper.edu.br>
2025-02-20 13:22:49 -0300

    ex2teste

--- exe2/main.c
+++ exe2/main.c
@@ -2,40 +2,40 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int LED_PIN = 25;   // LED onboard do RP2040
-const int BTN_PIN = 28;   // Pino do botão
+const int LED_PIN = 4;  
+const int BTN_PIN = 28;   
 
-volatile bool btn_pressed = false;  // Flag para sinalizar que o botão foi pressionado
+volatile bool btn_pressed = 1;  //Flag para sinalizar que o botão foi pressionado
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == GPIO_IRQ_EDGE_FALL) {
-        btn_pressed = true;
+    if (events == 0x4) {
+        btn_pressed = 0;
     }
 }
 
 int main() {
     stdio_init_all();
 
-    // Configura o LED como saída e o inicializa desligado
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 0);
-
-    // Configura o botão como entrada com pull-up
     gpio_init(BTN_PIN);
+    gpio_init(LED_PIN);
+  
     gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+  
     gpio_pull_up(BTN_PIN);
+  
 
-    // Habilita a interrupção apenas para borda de descida
-    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL, true, btn_callback);
 
     while (true) {
-        if (btn_pressed) {
-            btn_pressed = false;  // Reseta a flag
-            // Alterna o estado do LED
-            gpio_put(LED_PIN, !gpio_get(LED_PIN));
+        if (btn_pressed ==1) {
+          btn_pressed = 0;  
+          if (gpio_get(LED_PIN) == 0){
+            gpio_put(LED_PIN,1);
+          }else{
+            gpio_put(LED_PIN,0);
+          }
         }
-        sleep_ms(10);  // Pequena pausa para evitar busy waiting excessivo
-    }
-    return 0;
+
+      }
 }
